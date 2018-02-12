ad8b9288132eecbea7a455b7049b1b0c4de4b698
mrvklncr <mrvklncr@users.noreply.github.com>
2018-02-12 23:37:54 +0300

    Update aliens_sorter.py

--- aliens_sorter.py
+++ aliens_sorter.py
@@ -5,15 +5,15 @@ planetsList = list()
 header = True
 for line in aliens:
     if not header:
-        alien = line.split(",")[0]
-        planet = line.split(",")[1].replace("\n","")
+        alien = line.split(";")[0]
+        planet = line.split(";")[1].replace("\n","")
         aliensList.append(alien)
         planetsList.append(planet)
     else:
         header = False
 
 output = open('planets.csv',mode="w")
-output.write("species,planet\n")
+output.write("species;planet\n")
 
 planet_zerg = []
 planet_ho = []
@@ -21,11 +21,11 @@ planet_mars = []
 
 for alien in aliensList:
     planet = planetsList[aliensList.index(alien)]
-    if planet==" zerg-1":
+    if planet=="zerg-1":
         planet_zerg.append(alien)
-    if planet==" ho":
+    if planet=="ho":
         planet_ho.append(alien)
-    if planet==" mars":
+    if planet=="mars":
         planet_mars.append(alien)
 
 
@@ -34,21 +34,21 @@ class Zergalizer:
         transformed = list()
         for s in species:
             transformed.append(s.upper())
-        output.write(" ".join(transformed)+",zerg"+"\n")
+        output.write(" ".join(transformed)+";zerg"+"\n")
 
 class Hoizer:
     def do_hoizification(self, species):
         transformed = list()
         for s in species:
             transformed.append(s+"HO!")
-        output.write((" | ").join(transformed)+",ho"+"\n")
+        output.write((" | ").join(transformed)+";ho"+"\n")
 
 class Marsianizer:
     def do_marsianization(self, species):
         transformed = list()
         for s in species:
             transformed.append(" ".join(s.split("."))+"m")
-        output.write((" ~~ ").join(transformed)+",mars"+"\n")
+        output.write((" ~~ ").join(transformed)+";mars"+"\n")
 
 Zergalizer().do_zergalization(planet_zerg)
 Hoizer().do_hoizification(planet_ho)
